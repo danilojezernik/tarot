1a87b03ac93e4eb9f088c39b0a0606b0bd889b44
danilojezernik <dani.jezernik@gmail.com>
2023-03-12 16:02:13 +0100

    Commits: - TarotBranje: js (skripta za naključne slike ampak ne vedno enake) - slika vprašaj dodana

--- src/components/TarotBranje/tarotbranje.js
+++ src/components/TarotBranje/tarotbranje.js
@@ -9,27 +9,77 @@ import priestessDown from '@/assets/majorarcana/the-high-priestess-down.png'
 export default {
     data() {
         return {
-            slike: [fool, foolDown, magician, magicianDown, priestess, priestessDown],
+            slike: [
+                {
+                    id: 1,
+                    name: '0 - Norec',
+                    src: fool,
+                    to: "/the-fool"
+                },
+                {
+                    id: 1,
+                    name: '0 - Norec',
+                    src: foolDown,
+                    to: "/the-fool"
+                },
+                {
+                    id: 2,
+                    name: 'Magik',
+                    src: magician,
+                    to: "/the-magician"
+                },
+                {
+                    id: 2,
+                    name: 'Magik obrnjen',
+                    src: magicianDown,
+                    to: "/the-magician"
+                },
+                {
+                    id: 3,
+                    name: 'Svečenica',
+                    src: priestess,
+                    to: "/the-high-priestess"
+                },
+                {
+                    id: 3,
+                    name: 'Svečenica obrnjen',
+                    src: priestessDown,
+                    to: "/the-high-priestess"
+                }
+            ],
             selectedImage: '',
             threeSpread: 3,
+            showModal1: false,
+            showModal2: false,
+
         }
     },
-    created () {
+    created() {
         const idx = Math.floor(Math.random() * this.slike.length)
         this.selectedImage = this.slike[idx]
+        this.selectedImage = {
+            src: this.selectedImage.src,
+            name: this.selectedImage.name,
+            to: this.selectedImage.to
+        }
     },
     computed: {
         randomImageUrls() {
             const numImages = this.slike.length
-            const randomIndexes = []
+            const randomIds = []
 
-            while (randomIndexes.length < this.threeSpread) {
-                const randomIndex = Math.floor(Math.random() * numImages)
-                if (!randomIndexes.includes(randomIndex)) {
-                    randomIndexes.push(randomIndex)
+            while (randomIds.length < this.threeSpread) {
+                const randomId = this.slike[Math.floor(Math.random() * numImages)].id
+                if (!randomIds.includes(randomId)) {
+                    randomIds.push(randomId)
                 }
             }
-            return randomIndexes.map(index => this.slike[index])
+
+            return randomIds.map(id => {
+                const matchingImages = this.slike.filter(image => image.id === id)
+                const randomImage = matchingImages[Math.floor(Math.random() * matchingImages.length)]
+                return randomImage
+            })
         }
     }
 }
