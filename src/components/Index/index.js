7aed4e529debf8dd40c0f2074128024c1b86ce6d
danilojezernik <dani.jezernik@gmail.com>
2023-03-30 22:03:54 +0200

    Various commits

--- src/components/Index/index.js
+++ src/components/Index/index.js
@@ -1,33 +1,205 @@
+// Velika Arkana
 import fool from '@/assets/majorarcana/the-fool.png'
 import foolDown from '@/assets/majorarcana/the-fool-down.png'
 import magician from '@/assets/majorarcana/the-magician.png'
 import magicianDown from '@/assets/majorarcana/the-magician-down.png'
 import priestess from '@/assets/majorarcana/the-high-priestess.png'
 import priestessDown from '@/assets/majorarcana/the-high-priestess-down.png'
+import empress from '@/assets/majorarcana/the-empress.png'
+import empressDown from '@/assets/majorarcana/the-empress-down.png'
+import emperor from '@/assets/majorarcana/the-emperor.png'
+import emperorDown from '@/assets/majorarcana/the-emperor-down.png'
+import hierophant from '@/assets/majorarcana/the-hierophant.png'
+import hierophantDown from '@/assets/majorarcana/the-hierophant-down.png'
+import lovers from '@/assets/majorarcana/the-lovers.png'
+import loversDown from '@/assets/majorarcana/the-lovers-down.png'
+import chariot from '@/assets/majorarcana/the-chariot.png'
+import chariotDown from '@/assets/majorarcana/the-chariot-down.png'
+import strength from '@/assets/majorarcana/strength.png'
+import strengthDown from '@/assets/majorarcana/strength-down.png'
+import hermit from '@/assets/majorarcana/the-hermit.png'
+import hermitDown from '@/assets/majorarcana/the-hermit-down.png'
+import wheel from '@/assets/majorarcana/wheel-of-fortune.png'
+import wheelDown from '@/assets/majorarcana/wheel-of-fortune-down.png'
+import justice from '@/assets/majorarcana/justice.png'
+import justiceDown from '@/assets/majorarcana/justice-down.png'
+import hanged from '@/assets/majorarcana/the-hanged-man.png'
+import hangedDown from '@/assets/majorarcana/the-hanged-man-down.png'
+import death from '@/assets/majorarcana/death.png'
+import deathDown from '@/assets/majorarcana/death-down.png'
+import temperance from '@/assets/majorarcana/temperance.png'
+import temperanceDown from '@/assets/majorarcana/temperance-down.png'
+import devil from '@/assets/majorarcana/the-devil.png'
+import devilDown from '@/assets/majorarcana/the-devil-down.png'
+import tower from '@/assets/majorarcana/the-tower.png'
+import towerDown from '@/assets/majorarcana/the-tower-down.png'
+import star from '@/assets/majorarcana/the-star.png'
+import starDown from '@/assets/majorarcana/the-star-down.png'
+import moon from '@/assets/majorarcana/the-moon.png'
+import moonDown from '@/assets/majorarcana/the-moon-down.png'
+import sun from '@/assets/majorarcana/the-sun.png'
+import sunDown from '@/assets/majorarcana/the-sun-down.png'
+import judgement from '@/assets/majorarcana/judgement.png'
+import judgementDown from '@/assets/majorarcana/judgement-down.png'
+import world from '@/assets/majorarcana/the-world.png'
+import worldDown from '@/assets/majorarcana/the-world-down.png'
+
+// Palice
+import acewands from '@/assets/wands/ace-of-wands.png'
+import acewandsDown from '@/assets/wands/ace-of-wands-down.png'
+import twowands from '@/assets/wands/two-of-wands.png'
+import twowandsDown from '@/assets/wands/two-of-wands-down.png'
+import threewands from '@/assets/wands/three-of-wands.png'
+import threewandsDown from '@/assets/wands/three-of-wands-down.png'
+import fourwands from '@/assets/wands/four-of-wands.png'
+import fourwandsDown from '@/assets/wands/four-of-wands-down.png'
+import fivewands from '@/assets/wands/five-of-wands.png'
+import fivewandsDown from '@/assets/wands/five-of-wands-down.png'
+import sixwands from '@/assets/wands/six-of-wands.png'
+import sixwandsDown from '@/assets/wands/six-of-wands-down.png'
+import sevenwands from '@/assets/wands/seven-of-wands.png'
+import sevenwandsDown from '@/assets/wands/seven-of-wands-down.png'
+import eightwands from '@/assets/wands/eight-of-wands.png'
+import eightwandsDown from '@/assets/wands/eight-of-wands-down.png'
+import ninewands from '@/assets/wands/nine-of-wands.png'
+import ninewandsDown from '@/assets/wands/nine-of-wands-down.png'
+import tenwands from '@/assets/wands/ten-of-wands.png'
+import tenwandsDown from '@/assets/wands/ten-of-wands-down.png'
+import pagewands from '@/assets/wands/page-of-wands.png'
+import pagewandsDown from '@/assets/wands/page-of-wands-down.png'
+import knightwands from '@/assets/wands/knight-of-wands.png'
+import knightwandsDown from '@/assets/wands/knight-of-wands-down.png'
+import queenwands from '@/assets/wands/queen-of-wands.png'
+import queenwandsDown from '@/assets/wands/queen-of-wands-down.png'
+import kingwands from '@/assets/wands/king-of-wands.png'
+import kingwandsDown from '@/assets/wands/king-of-wands-down.png'
+
+// Kelihi
+import acecups from '@/assets/cups/ace-of-cups.png'
+import acecupsDown from '@/assets/cups/ace-of-cups-down.png'
+import twocups from '@/assets/cups/two-of-cups.png'
+import twocupsDown from '@/assets/cups/two-of-cups-down.png'
+import threecups from '@/assets/cups/three-of-cups.png'
+import threecupsDown from '@/assets/cups/three-of-cups-down.png'
+import fourcups from '@/assets/cups/four-of-cups.png'
+import fourcupsDown from '@/assets/cups/four-of-cups-down.png'
+import fivecups from '@/assets/cups/five-of-cups.png'
+import fivecupsDown from '@/assets/cups/five-of-cups-down.png'
+import sixcups from '@/assets/cups/six-of-cups.png'
+import sixcupsDown from '@/assets/cups/six-of-cups-down.png'
+import sevencups from '@/assets/cups/seven-of-cups.png'
+import sevencupsDown from '@/assets/cups/seven-of-cups-down.png'
+import eightcups from '@/assets/cups/eight-of-cups.png'
+import eightcupsDown from '@/assets/cups/eight-of-cups-down.png'
+import ninecups from '@/assets/cups/nine-of-cups.png'
+import ninecupsDown from '@/assets/cups/nine-of-cups-down.png'
+import tencups from '@/assets/cups/ten-of-cups.png'
+import tencupsDown from '@/assets/cups/ten-of-cups-down.png'
+import pagecups from '@/assets/cups/page-of-cups.png'
+import pagecupsDown from '@/assets/cups/page-of-cups-down.png'
+import knightcups from '@/assets/cups/knight-of-cups.png'
+import knightcupsDown from '@/assets/cups/knight-of-cups-down.png'
+import queencups from '@/assets/cups/queen-of-cups.png'
+import queencupsDown from '@/assets/cups/queen-of-cups-down.png'
+import kingcups from '@/assets/cups/king-of-cups.png'
+import kingcupsDown from '@/assets/cups/king-of-cups-down.png'
+
+// Meči
+import aceswords from '@/assets/swords/ace-of-swords.png'
+import aceswordsDown from '@/assets/swords/ace-of-swords-down.png'
+import twoswords from '@/assets/swords/two-of-swords.png'
+import twoswordsDown from '@/assets/swords/two-of-swords-down.png'
+import threeswords from '@/assets/swords/three-of-swords.png'
+import threeswordsDown from '@/assets/swords/three-of-swords-down.png'
+import fourswords from '@/assets/swords/four-of-swords.png'
+import fourswordsDown from '@/assets/swords/four-of-swords-down.png'
+import fiveswords from '@/assets/swords/five-of-swords.png'
+import fiveswordsDown from '@/assets/swords/five-of-swords-down.png'
+import sixswords from '@/assets/swords/six-of-swords.png'
+import sixswordsDown from '@/assets/swords/six-of-swords-down.png'
+import sevenswords from '@/assets/swords/seven-of-swords.png'
+import sevenswordsDown from '@/assets/swords/seven-of-swords-down.png'
+import eightswords from '@/assets/swords/eight-of-swords.png'
+import eightswordsDown from '@/assets/swords/eight-of-swords-down.png'
+import nineswords from '@/assets/swords/nine-of-swords.png'
+import nineswordsDown from '@/assets/swords/nine-of-swords-down.png'
+import tenswords from '@/assets/swords/ten-of-swords.png'
+import tenswordsDown from '@/assets/swords/ten-of-swords-down.png'
+import pageswords from '@/assets/swords/page-of-swords.png'
+import pageswordsDown from '@/assets/swords/page-of-swords-down.png'
+import knightswords from '@/assets/swords/knight-of-swords.png'
+import knightswordsDown from '@/assets/swords/knight-of-swords-down.png'
+import queenswords from '@/assets/swords/queen-of-swords.png'
+import queenswordsDown from '@/assets/swords/queen-of-swords-down.png'
+import kingswords from '@/assets/swords/king-of-swords.png'
+import kingswordsDown from '@/assets/swords/king-of-swords-down.png'
+
+// Pentaklji
+import acepentacles from '@/assets/pentacles/ace-of-pentacles.png'
+import acepentaclesDown from '@/assets/pentacles/ace-of-pentacles-down.png'
+import twopentacles from '@/assets/pentacles/two-of-pentacles.png'
+import twopentaclesDown from '@/assets/pentacles/two-of-pentacles-down.png'
+import threepentacles from '@/assets/pentacles/three-of-pentacles.png'
+import threepentaclesDown from '@/assets/pentacles/three-of-pentacles-down.png'
+import fourpentacles from '@/assets/pentacles/four-of-pentacles.png'
+import fourpentaclesDown from '@/assets/pentacles/four-of-pentacles-down.png'
+import fivepentacles from '@/assets/pentacles/five-of-pentacles.png'
+import fivepentaclesDown from '@/assets/pentacles/five-of-pentacles-down.png'
+import sixpentacles from '@/assets/pentacles/six-of-pentacles.png'
+import sixpentaclesDown from '@/assets/pentacles/six-of-pentacles-down.png'
+import sevenpentacles from '@/assets/pentacles/seven-of-pentacles.png'
+import sevenpentaclesDown from '@/assets/pentacles/seven-of-pentacles-down.png'
+import eightpentacles from '@/assets/pentacles/eight-of-pentacles.png'
+import eightpentaclesDown from '@/assets/pentacles/eight-of-pentacles-down.png'
+import ninepentacles from '@/assets/pentacles/nine-of-pentacles.png'
+import ninepentaclesDown from '@/assets/pentacles/nine-of-pentacles-down.png'
+import tenpentacles from '@/assets/pentacles/ten-of-pentacles.png'
+import tenpentaclesDown from '@/assets/pentacles/ten-of-pentacles-down.png'
+import pagepentacles from '@/assets/pentacles/page-of-pentacles.png'
+import pagepentaclesDown from '@/assets/pentacles/page-of-pentacles-down.png'
+import knightpentacles from '@/assets/pentacles/knight-of-pentacles.png'
+import knightpentaclesDown from '@/assets/pentacles/knight-of-pentacles-down.png'
+import queenpentacles from '@/assets/pentacles/queen-of-pentacles.png'
+import queenpentaclesDown from '@/assets/pentacles/queen-of-pentacles-down.png'
+import kingpentacles from '@/assets/pentacles/king-of-pentacles.png'
+import kingpentaclesDown from '@/assets/pentacles/king-of-pentacles-down.png'
 
 export default {
     data() {
         return {
-            karta: {},
-            slike: [fool, foolDown, magician, magicianDown, priestess, priestessDown],
-            selectedImage: '',
-            imageName: 'death-down.png'
+            slike: [fool, foolDown, magician, magicianDown, priestess, priestessDown, empress, empressDown, emperor,
+                emperorDown, hierophant, hierophantDown, lovers, loversDown, chariot, chariotDown, strength,
+                strengthDown, hermit, hermitDown, wheel, wheelDown, justice, justiceDown, hanged, hangedDown, death,
+                deathDown, temperance, temperanceDown, devil, devilDown, tower, towerDown, star, starDown, moon,
+                moonDown, sun, sunDown, judgement, judgementDown, world, worldDown, acewands, acewandsDown, twowands,
+                twowandsDown, threewands, threewandsDown, fourwands, fourwandsDown, fivewands, fivewandsDown, sixwands,
+                sixwandsDown, sevenwands, sevenwandsDown, eightwands, eightwandsDown, ninewands, ninewandsDown,
+                tenwands, tenwandsDown, pagewands, pagewandsDown, knightwands, knightwandsDown, queenwands,
+                queenwandsDown, kingwands, kingwandsDown, acecups, acecupsDown, twocups, twocupsDown, threecups,
+                threecupsDown, fourcups, fourcupsDown, fivecups, fivecupsDown, sixcups, sixcupsDown, sevencups,
+                sevencupsDown, eightcups, eightcupsDown, ninecups, ninecupsDown, tencups, tencupsDown, pagecups,
+                pagecupsDown, knightcups, knightcupsDown, queencups, queencupsDown, kingcups, kingcupsDown, aceswords,
+                aceswordsDown, twoswords, twoswordsDown, threeswords, threeswordsDown, fourswords, fourswordsDown,
+                fiveswords, fiveswordsDown, sixswords, sixswordsDown, sevenswords, sevenswordsDown, eightswords,
+                eightswordsDown, nineswords, nineswordsDown, tenswords, tenswordsDown, pageswords, pageswordsDown,
+                knightswords, knightswordsDown, queenswords, queenswordsDown, kingswords, kingswordsDown, acepentacles,
+                acepentaclesDown, twopentacles, twopentaclesDown, threepentacles, threepentaclesDown, fourpentacles,
+                fourpentaclesDown, fivepentacles, fivepentaclesDown, sixpentacles, sixpentaclesDown, sevenpentacles,
+                sevenpentaclesDown, eightpentacles, eightpentaclesDown, ninepentacles, ninepentaclesDown, tenpentacles,
+                tenpentaclesDown, pagepentacles, pagepentaclesDown, knightpentacles, knightpentaclesDown,
+                queenpentacles, queenpentaclesDown, kingpentacles, kingpentaclesDown],
+            currentImageIndex: 0,
         }
     },
     computed: {
-        imageSrc() {
-            return `http://localhost:5000/images/${this.imageName}`;
+        currentImageSrc() {
+            return this.slike[this.currentImageIndex];
         },
     },
-    created() {
-        const idx = Math.floor(Math.random() * this.slike.length)
-        this.selectedImage = this.slike[idx]
-    },
-    methods: {
-        currentDate() {
-            const current = new Date();
-            return `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
-        }
+    mounted() {
+        setInterval(() => {
+            this.currentImageIndex = Math.floor(Math.random() * this.slike.length);
+        }, 86400000);
     },
 }
 
--- src/components/VprInOdg/vno.js
+++ src/components/VprInOdg/vno.js
@@ -5,16 +5,29 @@ export default {
 
             accordions: [
                 {
-                    id: 1,
                     idHeading: 'flush-headingOne',
                     idCollapse: 'flush-collapseOne',
                     idCollapseL: '#flush-collapseOne',
+                    vpr: 'Katere tarot karte so uporabljene tukaj?',
+                    odg: 'Tukaj daj teks, ki je na strani tarot za raider weit',
+                },
+                {
+                    idHeading: 'flush-headingFive',
+                    idCollapse: 'flush-collapseFive',
+                    idCollapseL: '#flush-collapseFive',
+                    vpr: 'Ali lahko uporabljam katere druge karte?',
+                    odg: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a lectus ac turpis eleifend condimentum ac eget nisl. Aliquam erat volutpat. Aliquam tincidunt elementum enim, vitae placerat tortor ultrices et. Curabitur convallis ipsum elementum nunc tincidunt, quis tincidunt turpis maximus. Curabitur eget dignissim mi. Morbi quis erat eget ipsum porta facilisis. Fusce non lorem massa. In aliquam convallis dignissim. Nulla facilisi. Vestibulum rhoncus sapien ipsum, eget commodo lacus viverra faucibus.</p>' +
+                        '<p>Cras suscipit augue non nunc convallis, et gravida lorem porttitor. Maecenas fringilla lectus nec diam congue, ac fermentum est laoreet. Nullam interdum in arcu ac efficitur. Nam finibus mattis sapien, et posuere magna mattis elementum. Phasellus aliquam vulputate lorem non ultricies. Integer lobortis dolor ac malesuada dapibus. Etiam pulvinar nibh ac felis venenatis, vitae facilisis massa imperdiet.</p>',
+                },
+                {
+                    idHeading: 'flush-headingSix',
+                    idCollapse: 'flush-collapseSix',
+                    idCollapseL: '#flush-collapseSix',
                     vpr: 'Zakaj bi se sploh ukvarjal s tarotom?',
                     odg: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a lectus ac turpis eleifend condimentum ac eget nisl. Aliquam erat volutpat. Aliquam tincidunt elementum enim, vitae placerat tortor ultrices et. Curabitur convallis ipsum elementum nunc tincidunt, quis tincidunt turpis maximus. Curabitur eget dignissim mi. Morbi quis erat eget ipsum porta facilisis. Fusce non lorem massa. In aliquam convallis dignissim. Nulla facilisi. Vestibulum rhoncus sapien ipsum, eget commodo lacus viverra faucibus.</p>' +
                         '<p>Cras suscipit augue non nunc convallis, et gravida lorem porttitor. Maecenas fringilla lectus nec diam congue, ac fermentum est laoreet. Nullam interdum in arcu ac efficitur. Nam finibus mattis sapien, et posuere magna mattis elementum. Phasellus aliquam vulputate lorem non ultricies. Integer lobortis dolor ac malesuada dapibus. Etiam pulvinar nibh ac felis venenatis, vitae facilisis massa imperdiet.</p>',
                 },
                 {
-                    id: 2,
                     idHeading: 'flush-headingTwo',
                     idCollapse: 'flush-collapseTwo',
                     idCollapseL: '#flush-collapseTwo',
@@ -24,7 +37,6 @@ export default {
                         '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a lectus ac turpis eleifend condimentum ac eget nisl. Aliquam erat volutpat. Aliquam tincidunt elementum enim, vitae placerat tortor ultrices et. Curabitur convallis ipsum elementum nunc tincidunt, quis tincidunt turpis maximus. Curabitur eget dignissim mi. Morbi quis erat eget ipsum porta facilisis. Fusce non lorem massa. In aliquam convallis dignissim. Nulla facilisi. Vestibulum rhoncus sapien ipsum, eget commodo lacus viverra faucibus.</p>',
                 },
                 {
-                    id: 3,
                     idHeading: 'flush-headingThree',
                     idCollapse: 'flush-collapseThree',
                     idCollapseL: '#flush-collapseThree',
@@ -34,7 +46,6 @@ export default {
                         '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a lectus ac turpis eleifend condimentum ac eget nisl. Aliquam erat volutpat. Aliquam tincidunt elementum enim, vitae placerat tortor ultrices et. Curabitur convallis ipsum elementum nunc tincidunt, quis tincidunt turpis maximus. Curabitur eget dignissim mi. Morbi quis erat eget ipsum porta facilisis. Fusce non lorem massa. In aliquam convallis dignissim. Nulla facilisi. Vestibulum rhoncus sapien ipsum, eget commodo lacus viverra faucibus.</p>',
                 },
                 {
-                    id: 4,
                     idHeading: 'flush-headingFour',
                     idCollapse: 'flush-collapseFour',
                     idCollapseL: '#flush-collapseFour',
