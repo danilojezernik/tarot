00e5ef74310bc6fef90c16b4d2cfb10097cdf2d4
danilojezernik <dani.jezernik@gmail.com>
2023-04-08 12:49:02 +0200

    various changes

--- src/components/OpisTarotKarte/Meci/Desetica/desetica.js
+++ src/components/OpisTarotKarte/Meci/Desetica/desetica.js
@@ -5,23 +5,23 @@ export default {
             modrost: [
                 {
                     tarotKarta: "Desetica mečev",
-                    rekDesno: "VNOS<br>VNOS<br>VNOS",
+                    rekDesno: "Tema pred zoro<br>Kolaps zaradi stresov<br>Stres vas je podrl",
                 },
             ],
             opisKarte: [
                 {
-                    opisKarte: '',
+                    opisKarte: '<p>Desetica mečev prikazuje moškega, ki leži na tleh z obrazom navzdol, ki je očitno mrtev in z desetimi meči v hrbtu. Rdeča pelerina se prekriva na spodnji polovici njegovega telesa kot znak dostojanstva, ko zapušča ta svet. Medtem ko je temno nebo zlovešče, sonce vzhaja na obzorju in prinaša nov občutek upanja in priložnosti. Mirno morje v ozadju prinaša tudi tolažbo, saj daje slutiti, da je tudi v temi prisoten občutek miru in spokojnosti.</p>',
 
                     podnaslovPokoncna: "Pokončna karta",
-                    opisPokoncna: '',
+                    opisPokoncna: '<p>Desetica mečev označuje boleč, a neizogiben konec. Na primer, razmerje se lahko nenadoma konča, vašo službo lahko ukinejo ali pogodbo razdrejo. Kot da se je ta konec pojavil z jasnega in zamajal vaš svet. Tega nikoli niste mogli pričakovati, a zdaj se je zgodilo, presekalo vas je v bistvo in pustilo občutek, kot da se je svet sesul okoli vas. Žalujete zaradi te šokantne izgube in se sprašujete, ali boste kdaj spet ljubili ali našli delo ali zaupanje.</p><p>Ko se Desetica mečev pojavi v branju tarota, ste morda žrtev izdaje ali prevare druge osebe. Počutite se, kot da bi vas zabodli v hrbet in se otepate zaradi dejanj nekoga drugega. Morda vas je partner prevaral, kolega širi neprijetne govorice o vas ali pa je družinski član izdal vaše zaupanje. Povzročena bolečina je globoka, ne zato, ker je to, kar so storili, boleče, ampak zato, ker globoko v sebi veste, da to pomeni konec vašega odnosa, kot ga poznate z njimi. Poleg občutka bolečine lahko tudi žalujete zaradi izgube odnosa.</p><p>Desetica mečev lahko nakazuje, da prevzamete vlogo "žrtve" v upanju, da se vas bodo drugi usmilili in vas rešili obupa. Ne morete spremeniti dejanj druge osebe, lahko pa spremenite svoj odziv. Tudi če ste bili prizadeti ali izdani, imate zdaj možnost, da se poberete in nadaljujete s svojim življenjem, namesto da padete na tla v upanju, da se vas bo nekdo usmilil.</p><p>Na ta način Desetica mečev govori o opuščanju in sprejemanju trenutnih okoliščin. Ne upirate se več spremembam, temveč jim dovolite, da se zgodijo, tudi če vam povzročajo prvotno bolečino in trpljenje. Zavedate se, da mora priti do spremembe, da bi olajšali obnovo, in ji dovolite, da se zgodi, namesto da se z njo borite.</p><p>Dobra novica je, da Desetica mečev označuje zadnjo preizkušnjo – iz tega vira vas ne bo več doletela nobena bolečina. Ura je najtemnejša pred zoro in izkusiti morate polni vpliv tega, kar se je zgodilo, preden se lahko premaknete naprej in začnete znova. Bo lahko? Ne. Toda ali bo ta preizkušnja trajala večno? Ne.</p><p>Dvignite se s tal in razmislite o tem, kaj se vam je zgodilo in zakaj ter kaj se lahko naučite iz te izkušnje. Ko boste to storili, bosta bolečina in trpljenje zbledeli in kmalu boste videli, zakaj se je vse to moralo zgoditi, da se lahko razvijete v svoj največji potencial. Bolečina in rana, ki ste jo prestali, nista ostali brez namena. Uporabite pozitivno moč v sebi, da se učite iz svoje bolečine in črpate modrost iz poraza.</p>',
                     besedePokoncna: '',
 
                     podnaslovObrnjena: "Obrnjena karta",
-                    opisObrnjena: '',
+                    opisObrnjena: '<p>Obrnjena Desetica mečev pomeni, da se borite proti neizogibni spremembi ali koncu, ker se niste pripravljeni soočiti s tem, kar se dogaja. Vendar bolj ko se upirate, bolj se bo ta situacija vlekla, zato je čas, da strgate povoj (ne glede na to, kako boleče je to) in s tem končate, da lahko začnete na novo. Zaupajte, da se vse dogaja z razlogom, in čeprav je morda težko razumeti, skozi kaj greste, vedite, da to dolgoročno vodi k vaši osebni rasti in preporodu.</p><p>Podobno lahko obrnjena Desetica mečev predstavlja staro situacijo, ki se je slabo končala. Še vedno nosite s seboj rane, a ste jih zakopali globoko, da se tako ne zavedate, da so še vedno prisotne (in vas bolijo). S temi starimi bolečinami se je treba enkrat za vselej spopasti. Morda se bo težko poglobiti vase, vendar je to edini način, da se osvobodite te bolečine in ji dovolite, da odide iz vašega življenja.</p><p>Obrnjena Desetica mečev vas spodbuja, da ponovno ocenite svoje okoliščine in opustite vse vidike svojega življenja, ki vam ne služijo več. Namesto da razmišljate o svoji boleči preteklosti, je bistveno, da pogledate naprej in ugotovite, kako vas ti dogodki osvobodijo, da preoblikujete svoje življenje in izberete novo smer zase. Lahko se osvobodite svojo preteklost in ustvarite nov občutek sebe.</p><p>Končno se obrnjena Desetica mečev lahko pojavi kot dobrodošel znak, da se bolečina in žalost, ki ste jo čutili, končujeta. Ko so obrnjeni na glavo, se zdi, da meči v človekovem hrbtu padajo ven in ga osvobodijo bolečine, ki jo je utrpel. Osvobajate spomine na preteklost in si dopuščate priložnost, da greste naprej z občutkom prenove in upanja za prihodnost.</p>',
                     besedeObrnjena: '.',
 
                     podnaslovAfirmacija: "Afirmacija",
-                    afirmacija: "\"\"<br>",
+                    afirmacija: "\"Pripravljen/a sem osvoboditi preteklost in začeti znova.\"<br>",
                 }
             ],
             opisSimbolov: [
@@ -73,15 +73,15 @@ export default {
             elementi: [
                 {
                     ime: "DA ali NE vprašanje:",
-                    vrednost: "",
+                    vrednost: "NE",
                 },
                 {
                     ime: "Numerologija:",
-                    vrednost: "",
+                    vrednost: "10",
                 },
                 {
                     ime: "Element:",
-                    vrednost: "",
+                    vrednost: "Zrak",
                 },
                 {
                     ime: "Astrologija:",
--- src/components/OpisTarotKarte/Meci/Devetica/devetica.js
+++ src/components/OpisTarotKarte/Meci/Devetica/devetica.js
@@ -5,95 +5,87 @@ export default {
             modrost: [
                 {
                     tarotKarta: "Devetica mečev",
-                    rekDesno: "VNOS<br>VNOS<br>VNOS",
+                    rekDesno: "Doživljanje notranjega nemira<br>Anksioznost je visoka<br>Duševnih bolečin je na pretek",
                 },
             ],
             opisKarte: [
                 {
-                    opisKarte: '',
+                    opisKarte: '<p>Devetica mečev prikazuje žensko, ki sedi v svoji postelji z glavo v rokah. Zdi se, kot da se je prebudila iz strašne nočne more, zaradi katere je bila vznemirjena, prestrašena in zaskrbljena. Na temni steni za njo visi devet mečev, ki predstavljajo negativne misli, ki močno obremenjujejo njen um. Na dnu ženske postelje je izrezljana ena oseba, ki premaga drugo, vrtnice in obrisi astroloških simbolov pa krasijo odejo, ovito okoli nje.</p>',
 
                     podnaslovPokoncna: "Pokončna karta",
-                    opisPokoncna: '',
-                    besedePokoncna: '',
+                    opisPokoncna: '<p>Devetica mečev nakazuje, da vas obremenjujejo temne misli in moteči občutki (in vam ne dajo spati ponoči). Pretirano ste zaskrbljeni zaradi neke situacije in vaše negativne misli vas obvladujejo, zaradi česar ste pod stresom in zaskrbljeni. Bolj ko se povezujete s svojimi strahovi, bolj bodo vladali vašemu življenju. Ko ste obsedeni s tem, kar ne deluje, postajate vse bolj zaskrbljeni. Postane negativen cikel, kjer ena grozna misel vodi v drugo, in še eno, in še eno, dokler vas ne prevzamejo. Tako kot je devet mečev naloženih drug na drugega na steni, so tudi vaše negativne misli.</p><p>Bodite previdni, ker lahko strah in zaskrbljenost v Devetici mečev postaneta samouresničujoča se prerokba. Ko ste obsedeni s tem, kaj bi lahko šlo narobe, je večja verjetnost, da boste manifestirali svoj najslabši možni scenarij, ker nenamerno delujete tako, da podpirate svoje negativne misli. Na primer, morda vas skrbi, da vas bo partner prevaral, čeprav niste našli znakov nezvestobe. Bolj kot se obremenjujete s tem možnim izidom, bolj vaša tesnoba odganja vašega partnerja, dokler se nekega dne ne počuti tako ločenega od vas, da začne novo razmerje z nekom drugim. Ko v mislih ponavljate negativne misli, tvegate, da jih boste manifestirali v svojem življenju. Torej, Devetica mečev vas prosi, da prekinete ta krog. Vsakič, ko se pojavi negativno čustvo glejte nanj nevtralno in se ne poistovetite z njim in ga neprisilno zamenjajte s pozitivnim.</p><p>Devetica mečev vas opominja, da je velik del strahu in skrbi, ki ju doživljate, v vaši glavi in ni nujno točen odraz dogajanja okoli vas. Lahko si poslabšate stvari s pretiranim razmišljanjem ali obsedenostjo z najslabšimi scenariji. Čas je, da svojo situacijo preučite z nove perspektive. Namesto da iščete dokaze o tem, kaj gre narobe, se osredotočite na to, kaj je prav. Globoko vdihnite in spoznajte, da življenje ni tako grozno, kot se zdi, in da bolj ko vas skrbi, več škode si povzročate.</p><p>Devetica mečev se pogosto pojavi, ko vas skrbi, obup in tesnoba dobesedno ponoči držijo pokonci. Morda ne spite dobro ali pa imate pogoste nočne more. Vaše nenehne misli vam preprečujejo počitek.</p><p>Ko se v branju pojavi Devetica mečev, je čas, da se obrnete na pomoč. Tega vam ni treba storiti sam – pridobite podporo drugih, da vam bodo pomagali prebroditi to mračno obdobje vašega življenja. Drugi vam bodo pomagali videti luč na koncu tunela in vas popeljali do rešitve ali občutka miru kljub težavam. S težavo si jasno ogledate svojo situacijo, saj vas pestijo negativne misli, zato vam bo objektivna perspektiva tretje osebe pomagala najti izhod.</p>',
+                    besedePokoncna: 'Tesnoba, zaskrbljenost, strah, depresija, nočne more, trpljenje, dvom, opustošenje, bolezen, poškodba, smrt ljubljene osebe, sum, krutost, beda, izguba, nepoštenost, neusmiljenost, obrekovanje, kesanje, krivda, obžalovanje, brez veselja.',
 
                     podnaslovObrnjena: "Obrnjena karta",
-                    opisObrnjena: '',
-                    besedeObrnjena: '.',
+                    opisObrnjena: '<p>Obrnjena Devetica mečev kaže, da doživljate globok notranji nemir, ki je posledica vaše miselnosti. Vaše negativne misli vas popeljejo v padajočo spiralo obupa in tesnobe, ki je v nasprotju z resničnostjo; povzročate, da se stvari odvijajo veliko slabše, kot pa v resnici so.</p><p>Morda boste svoje skrbi poskušali obdržati zase, toda če se trudite obdržati glavo nad vodo, je zdaj morda čas, da se zaupate drugim in poiščete njihovo pomoč in podporo. Drugi vam bodo ponudili novo perspektivo ali celo samo prostor za izpuščanje, ki bo ublažil stres in napetost.</p><p>Obrnjena Devetica mečev se pojavi, ko vas zgrabi strah. Dovolite svojim strahom, da ovirajo vaš največji potencial in vam jemljejo vašo življenjsko moč. Ne morete narediti koraka naprej, ker ste tako prizadeti zaradi tega, kar se lahko zgodi. Preverite resničnost in ocenite, kateri strahovi so upravičeni in kateri ne. Sprostite svoja omejujoča prepričanja, hromeče strahove in dvom vase.</p><p>Obrnjena Devetica mečev lahko tudi nakazuje na to, da ste neverjetno strogi do sebe, da se ponižujete ali se ukvarjate z negativnim samogovorom. Torej, ko se v branju pojavi obrnjena Devetica mečev, se vprašajte, zakaj ste tako strogi do sebe. Kako se ponižujete in kaj je vir vaše depresije? Kaj lahko storite, da se boste počutili bolje? Morda se boste morali bolj zavestno potruditi, da opustite negativni samogovor in ga nadomestite s pozitivno okrepitvijo za dobro opravljeno delo.</p><p>Kar je bolj pozitivno, obrnjena Devetica mečev lahko nakazuje na to, da ste že preživeli to obdobje skrbi in depresije in sedaj okrevate. Morda ste ugotovili, da stvari niso tako slabe, kot ste si jih predstavljali, in se začenjate sproščati in pomirjati glede tega, kar je bila nekoč grozljiva situacija.</p>',
+                    besedeObrnjena: 'Notranji nemir, globoko zasidrani strahovi, skrivnosti, sproščanje skrbi, zdravljenje skozi čas, nesebičnost, potrpežljivost, luč na koncu tunela, obnovitev, izboljšanje težav.',
 
                     podnaslovAfirmacija: "Afirmacija",
-                    afirmacija: "\"\"<br>",
+                    afirmacija: "\"Soočam se s svojimi najglobljimi strahovi in jih zdravim.\"<br>",
                 }
             ],
             opisSimbolov: [
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Poza",
+                    opis: "Prizor nenadnega prebujenja ali šoka, teme ali osvetlitve."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Mrači se",
+                    opis: "Črna senca predstavljajo vse, kar je neznano - pa naj bo zato, ker je bilo potlačeno in pozabljeno ali zato, ker je izven vidnega, popolnoma novo ozemlje, nepredvidene naloge ali izkušnje."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Svetli meči",
+                    opis: "Bilo je temno. In v temi se prikaže luč - pravzaprav devet svetlobnih projektorjev, miselni prebliski navdiha, meči kot orožje intelekta. Vzemite si svoj čas, vendar poskrbite, da se boste navadili na nova spoznanja."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Roke pred obrazom I",
+                    opis: "Navajajte se na nova spoznanja in uvide. Vzemite si čas, ki je potreben. Sprostite se."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Roke pred obrazom II",
+                    opis: "Če je poza posledica zastrašujoče izkušnje, si prav tako vzemite čas. Celotno mentalno obzorje se zdaj kaže v novi luči. Ne zatiskajte si oči pred tem - to tukaj ne bo pomagalo."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Par dvojčkov",
+                    opis: "Negativno: notranja nesoglasja, bojevitost, samozavest, nepripravljenost videti drugo stran. Pozitivno: sprijazniti se z notranjim konfliktom, biti pripravljen za učenje in menjavo mest."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Odeja I",
+                    opis: "Vrtnice predstavljajo lepoto in pristnost notranjega jaza - kaj res premika vaše srce in kaj bi se v vas želelo uresničiti. Tukaj lahko najdete svojo točko odhoda in točko povratka."
                 },
                 {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Odeja II",
+                    opis: "Zbirka znamenj zodiaka in planetarnih simbolov nima posebnega astrološkega pomena. Bistvo je celoten kozmos, popoln cikel, ki ga je treba upoštevati kot integrirano celoto."
                 },
                 {
-                    simbol: "",
-                    opis: ""
-                },
-                {
-                    simbol: "",
-                    opis: ""
-                },
-                {
-                    simbol: "",
-                    opis: ""
+                    simbol: "Kontrastne barve črna in belo-modra",
+                    opis: "V biblijski zgodbi iz Geneze je Bog uporabil enega od šestih dni stvarjenja, da je prvotni kaos razvrstil v črno in belo. Za nas same ostaja veliko ustvarjalno dejanje ko nam uspe razvrstiti črno in belo!"
                 },
             ],
             elementi: [
                 {
                     ime: "DA ali NE vprašanje:",
-                    vrednost: "",
+                    vrednost: "NE",
                 },
                 {
                     ime: "Numerologija:",
-                    vrednost: "",
+                    vrednost: "9",
                 },
                 {
                     ime: "Element:",
-                    vrednost: "",
+                    vrednost: "Zrak",
                 },
                 {
                     ime: "Astrologija:",
-                    vrednost: "",
+                    vrednost: "Mars v dvojčku",
                 },
                 {
                     ime: "Kabalistični sefirot:",
-                    vrednost: "",
+                    vrednost: "Yesod (Temelj)",
                 },
                 {
                     ime: "Čas:",
-                    vrednost: ""
+                    vrednost: "Junij 1 - Junij 10"
                 }
             ],
         }
